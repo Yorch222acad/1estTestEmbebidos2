87d5c897ccef27f613807516a8f0cc47cabaf3a7
Jorge Salgueiro <yorch.academico@gmail.com>
2025-10-15 17:22:12 -0400

    ParteRasp

--- T1RPi4.py
+++ T1RPi4.py
@@ -2,6 +2,7 @@
 import RPi.GPIO as GPIO # GPIO
 import serial # Comunicación Serial
 from time import sleep # Retardo
+import pygame # Bluetooth
 
 # Variables:
 BtnBuzzer = 5 # Botón Buzzer
@@ -20,6 +21,13 @@ GPIO.setup(MdfDtCy, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Modificador Duty Cycle
 ser = serial.Serial('/dev/ttyACM0', 9600)
 ser.reset_input_buffer()
 
+# Configuración Bluetooth  :
+pygame.init()
+pygame.joystick.init()
+#------------------------------
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+
 # Función para ingresar el Duty Cycle por terminal:
 def ingDtCy():
     DtCy = int(input("Ingrese el valor del Duty Cycle entre 0 - 100: "))
@@ -27,6 +35,9 @@ def ingDtCy():
         print("Valor incorrecto")
         DtCy = int(input("Ingrese el valor del Duty Cycle entre 0 - 100: "))
     return DtCy
+
+print("Mando conectado:", joystick.get_name())
+print("Número de ejes detectados:", joystick.get_numaxes())
 
 while True:
     try:
@@ -54,6 +65,41 @@ while True:
             print("enviado:", mensaje.strip())
             sleep(0.2)
         #}--
+
+        #======================================================
+
+        pygame.event.pump()
+        eje_izq_y = joystick.get_axis(1)
+        eje_der_x = joystick.get_axis(3)
+        if eje_izq_y < -0.8 and eje_izq_y >= -1:
+            ser.write(b"adelante\n")
+            print(f"adelante")
+            det1 = False
+        elif eje_izq_y <= 1 and eje_izq_y > 0.8:
+            ser.write(b"atras\n")
+            print(f"atras")
+            det1 = False
+        else:
+            det1 = True
+        #------------------------------------------------
+        if eje_der_x < -0.8 and eje_der_x >= -1:
+            ser.write(b"izquierda\n")
+            print(f"izquierda")
+            det2 = False
+        elif eje_der_x <= 1 and eje_der_x > 0.8:
+            ser.write(b"derecha\n")
+            print(f"derecha")
+            det2 = False
+        else:
+            det2 = True
+        #------------------------------------------------
+        #------------------------------------------------
+        if det1 and det2:
+            ser.write(b"detenerse\n")
+            print(f"detenerse")
+        pygame.event.pump()
+        print(f"IZQ (X,Y): ({eje_izq_y:.2f}) | DER (X,Y): ({eje_der_x:.2f})")
+
         # Recibir el Duty Cycle modificado desde Tiva:
         if ser.in_waiting > 0:
             value = ser.readline().decode('utf-8').rstrip()
