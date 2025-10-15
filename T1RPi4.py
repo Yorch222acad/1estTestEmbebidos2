040284e3d785324d5079adaf6b30e6ce9606b564
Armevc09 <kevin.calani@ucb.edu.bo>
2025-10-14 20:07:18 -0400

    Integrate OpenCV for color detection and control
    Added video capture functionality using OpenCV to detect colors and send commands via UART.

--- T1RPi4.py
+++ T1RPi4.py
@@ -2,12 +2,22 @@
 import RPi.GPIO as GPIO # GPIO
 import serial # Comunicación Serial
 from time import sleep # Retardo
+import cv2
 
 # Variables:
 BtnBuzzer = 5 # Botón Buzzer
 BtnMtr1 = 6 # Botón Motor 1
 BtnMtr2 = 13 # Botón Motor 2
 MdfDtCy = 19 # Botón Modificar Duty Cycle
+
+
+droidcam_url = "http://172.20.10.3:4747/video"
+cap = cv2.VideoCapture(droidcam_url)
+
+if not cap.isOpened():
+    print("Error: No se pudo abrir la cámara.")
+    exit()
+
 
 # Configuración:
 GPIO.setmode(GPIO.BCM) # Pines (Modo de numeración BCM)
@@ -27,6 +37,9 @@ def ingDtCy():
         print("Valor incorrecto")
         DtCy = int(input("Ingrese el valor del Duty Cycle entre 0 - 100: "))
     return DtCy
+
+
+
 
 while True:
     try:
@@ -58,5 +71,43 @@ while True:
         if ser.in_waiting > 0:
             value = ser.readline().decode('utf-8').rstrip()
             print("DutyCycle recibido ", value)
+
+            ret, frame = cap.read()
+
+        if not ret:
+            print("Error: No se pudo obtener el fotograma.")
+            break
+        
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        
+        lower_red = (0, 100, 100)
+        upper_red = (10, 255, 255)
+        mask_red = cv2.inRange(hsv, lower_red, upper_red)
+        
+        lower_green = (50, 100, 100)
+        upper_green = (70, 255, 255)
+        mask_green = cv2.inRange(hsv, lower_green, upper_green)
+        
+        if cv2.countNonZero(mask_red) > 500:
+            ser.write(b"motor2\n")
+            print("enviado: Adelante")
+            sleep(0.2)
+        if cv2.countNonZero(mask_green) > 500:
+            ser.write(b"motor2\n")
+            print("enviado: Atras")
+            sleep(0.2)
+        
+        cv2.imshow('Droicam', frame)
+        
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+
+
     except Exception as e:
         print(e)
+
+    
+
+
+    
